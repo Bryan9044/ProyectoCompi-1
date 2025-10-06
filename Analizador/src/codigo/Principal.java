bd8c5acdd550ec38fabf28d2368f5b5867983058
Dylan Rodríguez <dylanrodriguezchavarria@gmail.com>
2025-10-05 20:18:15 -0600

    Se agrega cambio

--- Analizador/src/codigo/Principal.java
+++ Analizador/src/codigo/Principal.java
@@ -11,8 +11,8 @@ import java.io.File;
 
 public class Principal {
     public static void main(String[] args) throws Exception {
-        //JflexCup.generateFiles();
+        JflexCup.generateFiles();
         //JflexCup.analizarArchivo("C:/Users/bryan/Documents/Analizador/src/codigo/prueba.txt");
-        JflexCup.probarParser("C:/Users/bryan/Documents/Analizador/src/codigo/pruebaParser.txt");
+        //JflexCup.probarParser("C:/Users/bryan/Documents/Analizador/src/codigo/pruebaParser.txt");
     }
 }
